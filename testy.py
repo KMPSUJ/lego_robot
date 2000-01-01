c113bc4630c4f6150db8849faa52e20a11a36253
11111010000 <me2.7182@10g.pl>
2000-01-01 01:22:44 +0100

    tak trzeba robić obroty

--- testy.py
+++ testy.py
@@ -1,31 +1,43 @@
 from modules import Robot
-from time
-r = Robot()
-print(absolute 60deg)
+import time
+r = Robot.Robot()
+print ' absolute 60deg '
 r.M1.run_mode='position'
 r.M1.position_mode='absolute'
 r.M1.regulation_mode = True
-r.M1.pulses_per_second = 300
+r.M1.pulses_per_second_sp = 300
 r.M1.position_sp = r.M1.position + 60
 r.M1.start()
 r.T.play(440,100)
 time.sleep(1)
-print(absolute -60deg)
-r.M1.pulses_per_second = -300
+print 'absolute -60deg' 
+r.M1.pulses_per_second_sp = -300
 r.M1.position_sp = r.M1.position - 60
 r.M1.start()
 r.T.play(440,100)
 time.sleep(1)
-print(relative 60deg)
+print 'relative 60deg '
 r.M1.position_mode = 'relative'
 r.M1.position = 0
 r.M1.position_sp = 60 
 r.M1.start()
 r.T.play(440,100)
 time.sleep(1)
-print(relative -60deg)
+print 'relative -60deg'
 r.M1.position = 0
 r.M1.position_sp = -60 
 r.M1.start()
 r.T.play(440,100)
 time.sleep(1)
+
+r.M2.run_mode='position'
+r.M2.position_mode = 'relative'
+r.M2.regulation_mode = True
+r.M2.position = 0
+r.M1.position = 0
+r.M2.position_sp = 600 
+r.M1.position_sp = 600 
+r.M1.pulses_per_second_sp = 300
+r.M2.pulses_per_second_sp = 300
+r.M2.start()
+r.M1.start()
