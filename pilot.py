2bfcf0fdffde8b8e9b6bfd24b68dca3179390c58
11111010000 <me2.7182@10g.pl>
2000-01-01 02:17:17 +0100

    lepszy pilot

--- pilot.py
+++ pilot.py
@@ -6,14 +6,18 @@ while(True):
 	a = r.Read()
 	if not a[0] and not a[1]:
 		r.stop()
-	elif a[0] == 1:
+	if a[0] == 1 or a[0] == 5 or a[0] == 6:
 		r.A.run_forever(r.S/2)
-	elif a[0] == 2:
+	elif a[0] == 2 or a[0] == 7 or a[0] == 8:
 		r.A.run_forever(-r.S/2)
-	if a[0] == 3:
+	else:
+		r.A.stop()
+	if a[0] == 3 or a[0] == 5 or a[0] == 7:
 		r.C.run_forever(r.S/2)
-	elif a[0] == 4:
+	elif a[0] == 4 or a[0] == 6 or a[0] == 8:
 		r.C.run_forever(-r.S/2)
+	else:
+		r.C.stop()
 	if a[1] == 1:
 		r.B.run_forever(r.S) 
 	if a[1] == 2:
