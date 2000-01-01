f9fe957b1c0d3c71525f538c08e30ef45be26306
11111010000 <me2.7182@10g.pl>
2000-01-01 02:34:09 +0100

    Sterowanie pilotem

--- modules/Robot.py
+++ modules/Robot.py
@@ -27,7 +27,7 @@ class Robot():
 		
 		try:
 			self.A = Motor(port=Motor.PORT.A)
-			self.D = Motor(port=Motor.PORT.C)
+			self.C = Motor(port=Motor.PORT.C)
 			self.B = MMotor(port=MMotor.PORT.B)
 
 		except:
@@ -39,48 +39,52 @@ class Robot():
 			self.L.left.color = LED.COLOR.RED
 			self.say("Brak sensora dotyku", speed=200)
 		try:
-			C = ColorSensor()
+			self.Color = ColorSensor()
 		except:
 			self.L.left.color = LED.COLOR.RED
 			self.say("Brak sensora koloru", speed=200)
 		try:
-			IR = InfraredSensor()
-			IR.mode = "IR-REMOTE"
+			self.IR = InfraredSensor()
+			self.IR.mode = "IR-REMOTE"
 		except:
 			self.L.left.color = LED.COLOR.RED
 			self.say("Brak sensora podczerwieni",200)	
 
 		self.A.stop_mode = 'brake'     # robot stops in place
-		self.D.stop_mode = 'brake'     # robot stops in place
+		self.C.stop_mode = 'brake'     # robot stops in place
 		self.B.stop_mode = 'brake'
 		self.A.regulation_mode = True
-		self.D.regulation_mode = True
+		self.C.regulation_mode = True
 		self.B.regulation_mode = True
 		self.L.left.color = LED.COLOR.GREEN
 		self.L.right.color = LED.COLOR.GREEN
 		signal.signal(signal.SIGINT, self.sigint_handler)
 		# sets handler for keyboard interrupt( CTRL + C)
 
-	def Read():
-		return [IR.value0, IR.value1, IR.value2, IR.value3]
+	def Read(self):
+		return [self.IR.value0, self.IR.value1, self.IR.value2, self.IR.value3]
 		#valueN is channel N+1 
 		#1 LU
 		#2 Ld
 		#3 RU
 		#4 RD
+		#5 UU
+		#6 UD
+		#7 DU
+		#8 DD
 		#9 beacon
 
 	def run(self):
 		self.A.run_forever(self.S)
-		self.D.run_forever(self.S)
+		self.C.run_forever(self.S)
 
 	def run(self, value1=300, value2=300):
 		self.A.run_forever(value1)
-		self.D.run_forever(value2)
+		self.C.run_forever(value2)
 
 	def go(self):
 		self.A.start()
-		self.D.start()
+		self.C.start()
 
 	def check_obstacle(self):
 		return (self.touch.is_pushed and (not self.B))
@@ -90,7 +94,7 @@ class Robot():
 
 	def stop(self, val=0):
 		self.A.stop()
-		self.D.stop()
+		self.C.stop()
 		self.B.stop()
 
 	def start(self):
@@ -101,7 +105,7 @@ class Robot():
 	def shutdown(self):
 		self.stop()
 		self.A.stop_mode = 'coast'     # free motor
-		self.D.stop_mode = 'coast'     # free motor
+		self.C.stop_mode = 'coast'     # free motor
 		self.B.stop_mode = 'coast'
 		self.L.left.color = LED.COLOR.AMBER
 		self.L.left.on()
@@ -125,25 +129,25 @@ class Robot():
 
 	def turn(self, value=1, callibrate=1):
 		self.A.set_rel_position(2248*value*callibrate, speed_sp=self.S)
-		self.D.set_rel_position(-2248*value*callibrate, speed_sp=-self.S)
+		self.C.set_rel_position(-2248*value*callibrate, speed_sp=-self.S)
 		self.go()
 
 	def run_dist(self, value=1):
 		self.A.set_rel_position(720*value, speed_sp=self.S)
-		self.D.set_rel_position(720*value, speed_sp=self.S)
+		self.C.set_rel_position(720*value, speed_sp=self.S)
 		self.go()
 
 	def add_dist(self, value):
 		self.A.add_rel_position(value)
-		self.D.add_rel_position(value)
+		self.C.add_rel_position(value)
 
 	def add_turn(self, value, callibrate=1):
 		self.A.add_rel_position(2248*value*callibrate)
-		self.D.add_rel_position(-2248*value*callibrate)
+		self.C.add_rel_position(-2248*value*callibrate)
 
 	def left_dist(self):
 		return (self.A.position - self.A.position_sp,
-			self.D.position - self.D.position_sp)
+			self.C.position - self.C.position_sp)
 
 	def set_speed(self, val=600):
 		if val > 800:
--- pilot.py
+++ pilot.py
@@ -1,14 +1,22 @@
-# coding: utf-8
-from ev3.lego import InfraredSensor
-d = InfraredSensor()
-d.mode = "IR-REMOTE"
-def Read():
-    a = [d.value0, d.value1, d.value2, d.value3]
-    return a
-Read()[1]
-#valueN, to N+1 kanał
-#1 LG
-#2 LD
-#3 PG
-#4 PD
-#9 beacon
+from modules import Robot
+import time
+
+r = Robot.Robot()
+while(True):
+	a = r.Read()
+	if not a[0] and not a[1]:
+		r.stop()
+	elif a[0] == 1:
+		r.A.run_forever(r.S/2)
+	elif a[0] == 2:
+		r.A.run_forever(-r.S/2)
+	if a[0] == 3:
+		r.C.run_forever(r.S/2)
+	elif a[0] == 4:
+		r.C.run_forever(-r.S/2)
+	if a[1] == 1:
+		r.B.run_forever(r.S) 
+	if a[1] == 2:
+		r.B.run_forever(-r.S) 
+		
+		
