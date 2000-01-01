63037dafaa0a8a553e222c4e73424f0bc6d67b63
11111010000 <me2.7182@10g.pl>
2000-01-01 01:04:59 +0100

    Test na fugach

--- fugi.py
+++ fugi.py
@@ -7,8 +7,8 @@ R = r.Robot()
 R.startup()
 R.start()
 R.S = 200
-# R.run()
-R.turn(2)
+R.run_dist(20)
+# R.turn(2)
 s = open("out",'a')
 st = ''
 s.write(str(datetime.datetime.today()))
@@ -18,15 +18,18 @@ while(True):
     if ref[1] < 145:
         R.T.play(440,80)
     st += (str(ref))
-    st += str(datetime.datetime.now().isoformat())
+    st += str(time.time())
+    t = time.time()
     st += ('\n')
-    if(R.check_obstacle()):
+
+    if(k > 10 or R.K.backspace):
+        s.write(st)
+        s.close()
+        R.shutdown()
+''' if(R.check_obstacle()):
         R.obstacle()
         k = k+1
     else:
         R.start()
         #     k = 0
-    if(k > 10 or R.K.backspace):
-        s.write(st)
-        s.close()
-        R.shutdown()
+''' 
