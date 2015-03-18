6164d817a19d690c4e526a5c6994b5cb6f20b7b1
milox93 <bratek.jj@gmail.com>
2015-03-18 16:11:42 +0100

    Jazda stop 1

---
+++ jazdastop.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+from ev3.lego import TouchSensor
+from ev3.ev3dev import Motor
+from ev3.ev3dev import LED
+import unittest
+import time
+
+d = TouchSensor()
+m = Motor(port=Motor.PORT.A)
+k = 0
+l = LED()
+m.run_forever(-50)
+
+m.stop_mode = 'brake'
+
+while(True):
+    time.sleep(.1)
+    if(d.is_pushed):
+      m.stop()
+      k = k+1
+    else:
+      m.start()
+      k = 0
+    if(k > 10):
+      l.left.color=LED.COLOR.AMBER
+      l.left.on()
+      break
+l.left.off()
