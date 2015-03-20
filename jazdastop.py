37d439a60f5022aab842ab9cc7e547f3e2400524
11111010000 <me2.7182@10g.pl>
2015-03-20 12:18:34 +0100

    po testach

--- jazdastop.py
+++ jazdastop.py
@@ -8,17 +8,20 @@ import time
 import signal
 import sys
 
+
+d = TouchSensor()
+m = Motor(port=Motor.PORT.A)
+k = 0
+K = Key()
+l = LED()
+t= Tone()
+ 
 def startup_sequence():
-    global d = TouchSensor()
-    global m = Motor(port=Motor.PORT.A)
-    global k = 0
-    global K = Key()
-    global l = LED()
-    m.run_forever(-50)
-    m.stop_mode = 'brake'
-    l.left.color=LED.COLOR.GREEN
-    l.right.color=LED.COLOR.GREEN
-    signal.signal(signal.SIGINT, sigint_handler)  #sets handler for keyboard interrupt( CTRL + C)
+   m.run_forever(-50)
+   m.stop_mode = 'brake'
+   l.left.color=LED.COLOR.GREEN
+   l.right.color=LED.COLOR.GREEN
+   signal.signal(signal.SIGINT, sigint_handler)  #sets handler for keyboard interrupt( CTRL + C)
 
 def sigint_handler(signal, frame): #defines interrupt handler
     shutdown_sequence()
@@ -28,10 +31,11 @@ def shutdown_sequence():
     m.stop_mode = 'coast'
     l.left.color=LED.COLOR.AMBER
     l.left.on()
-    Tone.play(440)
-    sleep(0.1)
+    t.play(440)
+    time.sleep(0.1)
     l.left.color=LED.COLOR.GREEN
-    Tone.stop()
+    l.left.on()
+    t.stop()
     sys.exit(0)
 
 #end of definitions
@@ -39,10 +43,14 @@ startup_sequence()
 while(True):
     time.sleep(.1)
     if(d.is_pushed):
+      l.left.color=LED.COLOR.YELLOW
+      l.left.on()
       m.stop()
       k = k+1
     else:
+      l.left.color=LED.COLOR.GREEN
+      l.left.on()
       m.start()
       k = 0
-    if(k > 10 or K.backspace)):
+    if(k > 10 or K.backspace):
       shutdown_sequence()
